4a56c64a08cf1a973e1d5508bc39387c66d2861b
Chris Glass <christopher.glass@divio.ch>
2010-10-20 10:58:50 +0200

    - Second part of the massive rename to cmsplugin_facebook

---
+++ cmsplugin_facebook/cms_plugins.py
@@ -0,0 +1,29 @@
+from cms.plugin_base import CMSPluginBase
+from cms.plugin_pool import plugin_pool
+from cms_facebook import models
+
+class BasePlugin(CMSPluginBase):
+    name = None
+    
+    def render(self, context, instance, placeholder):
+        context.update({'instance': instance, 'name': self.name})
+        return context
+
+class LikeBoxPlugin(BasePlugin):
+    model = models.LikeBox
+    name = 'Facebook LikeBox Plugin'
+    render_template = 'cmsplugin_facebook/likebox.html'
+        
+class ShareButtonPlugin(BasePlugin):
+    model = models.ShareButton
+    name = 'Facebook "Share" button plugin'
+    render_template = "cmsplugin_facebook/sharebutton.html"
+
+class LikeButtonPlugin(BasePlugin):
+    model = models.LikeButton
+    name = 'Facebook Like Button Plugin'
+    render_template = 'cmsplugin_facebook/likebutton.html'
+    
+plugin_pool.register_plugin(LikeBoxPlugin)
+plugin_pool.register_plugin(LikeButtonPlugin)
+plugin_pool.register_plugin(ShareButtonPlugin)
