48ee3f951eaed8578fa3f59e1f40d4d1a1e6b87a
Chris Glass <christopher.glass@divio.ch>
2010-10-20 16:40:33 +0200

    - Fixed setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ version = __import__('cmsplugin_facebook').__version__
 setup(
     name = 'cmsplugin_facebook',
     version = version,
+    license= 'BSD',
     description = 'Django CMS Facebook Plugins',
     author = 'Christopher Glass, Jonas Obrist',
     author_email = 'christopher.glass@divio.ch, jonas.obrist@divio.ch',
@@ -16,7 +17,13 @@ setup(
         ]
     },
     zip_safe=False,
-    install_requires=[
-        "django-cms",
+    classifiers = [
+        'Development Status :: 4 -Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
     ]
 )
